aea0cbe45635280fac5405b71294f4a9663f9ff8
Caiden Sanders <work.caidensanders@gmail.com>
2021-01-16 23:21:58 -0800

    Fixed import in MoonBackground.tsx

--- packages/web-frontend/src/pages/Home/components/MoonBackground/MoonBackground.tsx
+++ packages/web-frontend/src/pages/Home/components/MoonBackground/MoonBackground.tsx
@@ -9,7 +9,7 @@
 import styled from 'styled-components';
 
 // React Components
-import MoonlightImage from './MoonlightImage';
+import MoonlightImage from 'pages/Home/components/MoonBackground/MoonlightImage';
 
 // MoonBackground CSS Element
 const MoonlightWrapper = styled.div`
