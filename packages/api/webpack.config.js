80d0e09f681498393fffad6c91677d08591a9ba2
Caiden Sanders <work.caidensanders@gmail.com>
2021-01-19 11:13:31 -0800

    packages/api changed entry to correct directory webpack.config.js

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/server.js',
+  entry: './dist/src/server.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
